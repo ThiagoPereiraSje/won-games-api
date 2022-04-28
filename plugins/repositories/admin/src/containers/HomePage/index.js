d7d547c349a79f7fd4cca93692d65f9f7d84095e
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-28 11:30:56 -0300

    Add the plugin content static

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,68 @@
-/*
- *
- * HomePage
- *
- */
-
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+import styled from 'styled-components';
+
+const headers = [
+  {
+    name: 'Name',
+    value: 'name',
+  },
+  {
+    name: 'Description',
+    value: 'description',
+  },
+  {
+    name: 'Url',
+    value: 'html_url',
+  },
+];
+
+const rows = [
+  {
+    name: 'landing-page',
+    description: 'Code to the sales landing page.',
+    html_url: 'https://github.com/React-Avancado/landing-page',
+  },
+  {
+    name: 'links-estudo',
+    description: 'Links interessantes sobre tudo abordado no curso.',
+    html_url: 'https://github.com/React-Avancado/links-estudo',
+  },
+  {
+    name: 'boilerplate',
+    description: 'Boilerplate to use in our React Avançado course.',
+    html_url: 'https://github.com/React-Avancado/boilerplate',
+  },
+  {
+    name: 'landing-page-api',
+    description: 'API made with Strapi to seed the data our NextJS Landing Page.',
+    html_url: 'https://github.com/React-Avancado/landing-page-api',
+  },
+  {
+    name: 'reactavancado-extension-pack',
+    description: 'A collection of extension that we use at ReactAvancado.com.br course.',
+    html_url: 'https://github.com/React-Avancado/reactavancado-extension-pack',
+  },
+];
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Wrapper>
+      <Header
+        title={{ label: 'React Avançado Repositories' }}
+        content="A list of our repositories in React Avançado course" />
+
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
   );
 };
 
